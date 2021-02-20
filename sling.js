6d070a2d796ea4ad292d506aba2c3c378ff11bf4
sachinchavanlifetimephotos <75015277+sachinchavanlifetimephotos@users.noreply.github.com>
2021-02-20 15:57:39 +0530

    Add files via upload

---
+++ sling.js
@@ -0,0 +1,14 @@
+class sling{ 
+    constructor(bodyA, pointB){ 
+        var options={ 
+            bodyA:bodyA, 
+            pointB:pointB, 
+            stiffness:0.04, 
+            length:10
+        } 
+        
+         this.sling=Constraint.create(options); 
+        World.add(world,this.sling) 
+
+    } 
+}
